ec820b9672f045e0b3e09c0f301e0245b2a076ed
Kareem Al-Skafi <80727181+kareemalskafi@users.noreply.github.com>
2023-11-30 16:39:34 +0300

    Add project files.

---
+++ PFPE_CSharp_Tasks.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PFPE_CSharp_Tasks.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
